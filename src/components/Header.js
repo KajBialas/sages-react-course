5f600f412734862454d37cd33fb4ca8a3f8f4e5b
Kaj Białas <kaj.bialas@dev-industry.com>
2020-05-06 15:07:26 +0200

    issue11 init

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
   return(
-    <header>Moja aplikacja</header>
+    <header className={?}>Moja aplikacja</header>
   )
 }
 
