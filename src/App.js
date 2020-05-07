bf0a06790434c98b1e0d971b60ce6083a89edafa
Kaj Białas <kaj.bialas@dev-industry.com>
2020-05-07 16:57:22 +0200

    Add user context

--- src/App.js
+++ src/App.js
@@ -6,39 +6,33 @@ import Page404 from './pages/404';
 import Post from './pages/Post';
 import Footer from './components/Footer';
 import Menu from './components/Menu';
+import UserLogin from './components/userLogin/UserLogin.component';
 
 import { COUNTER_INITIAL_STATE, counterReducer} from './components/counter/Counter.reducer';
 
 export const CounterContext = React.createContext(1);
+export const UserContext = React.createContext('');
 
 function App() {
   const [state, dispatch] = useReducer(counterReducer, COUNTER_INITIAL_STATE);
   const [userName, setUserName] = useState('');
 
-  const [userNameForm, setUserNameForm] = useState('');
-
   return(
     <CounterContext.Provider value={[state, dispatch]}>
-      <BrowserRouter>
-
-        <div>
-          <h2>Logowanie</h2>
-          { userName ? <div>Użytkownik zalogowany</div> : <form onSubmit={() => setUserName(userNameForm)}>
-            <input type="text" value={userNameForm} onChange={(e) => setUserNameForm(e.target.value)} />
-          </form> }
-        </div>
-
-
-        <Menu />
-        <Switch>
-          <Redirect from="/home" to="/" />
-          <Route exact path="/" component={Home} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/post/:id" component={Post} />
-          <Route component={Page404} />
-        </Switch>
-        <Footer text="Copyright 20020" />
-      </BrowserRouter>
+      <UserContext.Provider value={[userName, setUserName]}>
+        <BrowserRouter>
+          <UserLogin />
+          <Menu />
+          <Switch>
+            <Redirect from="/home" to="/" />
+            <Route exact path="/" component={Home} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/post/:id" component={Post} />
+            <Route component={Page404} />
+          </Switch>
+          <Footer text="Copyright 20020" />
+        </BrowserRouter>
+      </UserContext.Provider>
     </CounterContext.Provider>
   )
 }
